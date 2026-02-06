9019b9407590f72f5dcdbe30771c2d9a87d9d8f2
Harun Ayartürk <harunayarturk@gmail.com>
2026-02-06 07:44:10 +0100

    Add unit tests

---
+++ test_product.py
@@ -0,0 +1,38 @@
+import pytest
+from products import Product
+
+
+def test_creating_prod():
+    p = Product("MacBook Air M2", price=1450, quantity=100)
+    assert p.name == "MacBook Air M2"
+    assert p.price == 1450
+    assert p.get_quantity() == 100
+    assert p.is_active() is True
+
+
+def test_creating_prod_invalid_details():
+    with pytest.raises(Exception):
+        Product("", price=1450, quantity=100)
+
+    with pytest.raises(Exception):
+        Product("MacBook Air M2", price=-10, quantity=100)
+
+
+def test_prod_becomes_inactive():
+    p = Product("MacBook Air M2", price=1450, quantity=1)
+    p.buy(1)
+    assert p.get_quantity() == 0
+    assert p.is_active() is False
+
+
+def test_buy_modifies_quantity():
+    p = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    total = p.buy(3)
+    assert total == 750
+    assert p.get_quantity() == 497
+
+
+def test_buy_too_much():
+    p = Product("Google Pixel 7", price=500, quantity=2)
+    with pytest.raises(Exception):
+        p.buy(3)
